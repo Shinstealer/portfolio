ee631aeeb5e0672fba6eda227a59c7c85fa3bb82
501-19 <501-19@501-19-PC>
2018-10-02 14:52:46 +0900

    insert done

--- sungminkim/src/controller/PortfolioDAO.java
+++ sungminkim/src/controller/PortfolioDAO.java
@@ -12,31 +12,29 @@ public class PortfolioDAO {
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
 	ArrayList<PortfolioDTO> list = new ArrayList<>();
+	int result = 0;
 	
-	public void conInsert(PortfolioDTO pDto) {
+	public int conInsert(PortfolioDTO pDto) {
 		try {
 			conn = DBManager.getConnection();
-			String sql = "insert into contactlist(rno,gname,email,message) "
-					+ "values(seq_rno.nextval,?,?,?)";
+			String sql = "INSERT INTO contactlist(rno,gname,email,message) "
+					+ "VALUES(seq_rno.nextval,?,?,?)";
 			
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1,pDto.getGname());
 			pstmt.setString(2,pDto.getEmail());
 			pstmt.setString(3,pDto.getMessage());
 			
-			int result = pstmt.executeUpdate();
+			result = pstmt.executeUpdate();
 			
-			if(result > 0 ) {
-				System.out.println("등록 성공");
-			}else {
-				System.out.println("등록 실패");
-			}
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}finally {
 			DBManager.close(conn, pstmt);
 		}
+		return result;
 	}
 	
 }
